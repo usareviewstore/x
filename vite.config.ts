ebc8c530c8f4f36160802a3ddaa7c57e80d69b2e
usareviewstore <usareviewstores@gmail.com>
2026-08-06 18:39:19 +0000

    Fix SPA double slash redirect bug and set relative base paths for custom domain and github.io

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.VITE_BASE || '/',
+    base: process.env.VITE_BASE || './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
